5ebe134ca1469d41f89260f674da48681411cd3e
deonis <denus.reshetnuk@gmail.com>
2020-08-26 06:25:01 +0300

    db update
    hope the last

--- App/consts.php
+++ App/consts.php
@@ -4,6 +4,6 @@ define('AUTOLOAD', require_once '../vendor/autoload.php');
 define('ROOTHPATH', __DIR__);
 
 define('DB_HOST', 'us-cdbr-east-02.cleardb.com');
-define('DB_USER', 'heroku_47804dc89acd860');
-define('DB_PASS', 'b6d8679c20b9f4');
+define('DB_USER', 'b6d8679c20b9f4');
+define('DB_PASS', 'heroku_47804dc89acd860');
 define('DB_NAME', '5c2f5587');
