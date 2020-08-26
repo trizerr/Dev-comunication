cc9cb2c254233864d65e4d4245ef745657ab7589
deonis <denus.reshetnuk@gmail.com>
2020-08-26 06:26:42 +0300

    db fix

--- App/consts.php
+++ App/consts.php
@@ -5,5 +5,5 @@ define('ROOTHPATH', __DIR__);
 
 define('DB_HOST', 'us-cdbr-east-02.cleardb.com');
 define('DB_USER', 'b6d8679c20b9f4');
-define('DB_PASS', 'heroku_47804dc89acd860');
-define('DB_NAME', '5c2f5587');
+define('DB_PASS', '5c2f5587');
+define('DB_NAME', 'heroku_47804dc89acd860');
